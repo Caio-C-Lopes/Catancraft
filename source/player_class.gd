ec9094201386d79f883d373c3d522a0310c39bb8
Vitor Lemos <vt.lemos1701@gmail.com>
2026-05-11 01:21:03 -0300

    feat: adding player class

---
+++ source/player_class.gd
@@ -0,0 +1,30 @@
+class_name Player
+
+var player_name: String
+var player_color: Color
+var ponits: int
+
+var resources := {
+	'wood': 0,
+	'brick': 0,
+	'wheat': 0,
+	'sheep': 0,
+	'ore': 0
+}
+
+func _init(name: String, color: Color):
+	player_name = name
+	player_color = color
+	
+func add_resource(resource: String, amount: int):
+	if resources.has(resource):
+		resources[resource] += amount
+
+func remove_resource(resource: String, amount: int):
+	if resources.has(resource):
+		resources[resource] -= amount
+		
+func can_afford(cost: Dict):
+	for i in cost:
+		if resources
+		
