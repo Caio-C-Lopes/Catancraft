93f40f6d76e609d212aa806ec5121160a74c4f0d
lexguimaraes <alexfsg@id.uff.br>
2026-05-11 20:20:09 -0300

    chore(formatting): format source and test files

--- source/board.gd
+++ source/board.gd
@@ -54,31 +54,31 @@ func _ready():
 
 func shuffle_valid_board():
 	var adjacencies = [
-		[1, 3, 4], # 0
-		[0, 2, 4, 5], # 1
-		[1, 5, 6], # 2
-		[0, 4, 7, 8], # 3
-		[0, 1, 3, 5, 8, 9], # 4
-		[1, 2, 4, 6, 9, 10], # 5
-		[2, 5, 10, 11], # 6
-		[3, 8, 12], # 7
-		[3, 4, 7, 9, 12, 13], # 8
-		[4, 5, 8, 10, 13, 14], # 9
-		[5, 6, 9, 11, 14, 15], # 10
-		[6, 10, 15], # 11
-		[7, 8, 13, 16], # 12
-		[8, 9, 12, 14, 16, 17], # 13
-		[9, 10, 13, 15, 17, 18], # 14
-		[10, 11, 14, 18], # 15
-		[12, 13, 17], # 16
-		[13, 14, 16, 18], # 17
-		[14, 15, 17] # 18
+		[1, 3, 4],  # 0
+		[0, 2, 4, 5],  # 1
+		[1, 5, 6],  # 2
+		[0, 4, 7, 8],  # 3
+		[0, 1, 3, 5, 8, 9],  # 4
+		[1, 2, 4, 6, 9, 10],  # 5
+		[2, 5, 10, 11],  # 6
+		[3, 8, 12],  # 7
+		[3, 4, 7, 9, 12, 13],  # 8
+		[4, 5, 8, 10, 13, 14],  # 9
+		[5, 6, 9, 11, 14, 15],  # 10
+		[6, 10, 15],  # 11
+		[7, 8, 13, 16],  # 12
+		[8, 9, 12, 14, 16, 17],  # 13
+		[9, 10, 13, 15, 17, 18],  # 14
+		[10, 11, 14, 18],  # 15
+		[12, 13, 17],  # 16
+		[13, 14, 16, 18],  # 17
+		[14, 15, 17]  # 18
 	]
 
 	while true:
 		available_resources.shuffle()
 		available_numbers.shuffle()
-		
+
 		var hex_numbers = []
 		var number_idx = 0
 		for r in available_resources:
@@ -87,9 +87,9 @@ func shuffle_valid_board():
 			else:
 				hex_numbers.append(available_numbers[number_idx])
 				number_idx += 1
-		
+
 		var is_valid = true
-		
+
 		for i in range(19):
 			var num_i = hex_numbers[i]
 			if num_i == 6 or num_i == 8:
@@ -100,10 +100,10 @@ func shuffle_valid_board():
 						break
 			if not is_valid:
 				break
-				
+
 		if not is_valid:
 			continue
-			
+
 		var red_counts = {}
 		for i in range(19):
 			var num_i = hex_numbers[i]
@@ -115,7 +115,7 @@ func shuffle_valid_board():
 				if red_counts[type] > 1:
 					is_valid = false
 					break
-		
+
 		if is_valid:
 			break
 
