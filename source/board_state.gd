63afec3cfe160e3cc53c38755eb4ef08ca994920
Molgo <miguelramos5gg@gmail.com>
2026-05-12 08:03:39 -0300

    chore(formatting): improve code formatting in board_state.gd

--- source/board_state.gd
+++ source/board_state.gd
@@ -1,23 +1,21 @@
 extends Node
 
-enum BuildingType {NONE, VILLAGE, CITY}
+enum BuildingType { NONE, VILLAGE, CITY }
 
 var vertices: Dictionary = {}
 var edges: Dictionary = {}
 
+
 func register_vertices(pos: Vector2):
 	var key = Vector2(round(pos.x), round(pos.y))
 	if not vertices.has(key):
-		vertices[key] = {
-			"owner": null,
-			"type": BuildingType.NONE,
-			"links": []
-		}
+		vertices[key] = {"owner": null, "type": BuildingType.NONE, "links": []}
+
 
 func register_edges(a: Vector2, b: Vector2):
 	var center = (a + b) / 2.0
 	var key = Vector2(round(center.x), round(center.y))
-	
+
 	if not edges.has(key):
 		edges[key] = {
 			"owner": null,
