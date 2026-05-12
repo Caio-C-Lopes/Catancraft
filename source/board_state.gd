07c97f16eccf884991d6a545e2aa2c7dec9b5924
Gabriel Baeta <gabrielbaeta@id.uff.br>
2026-05-12 18:41:22 -0300

    fix: test error

--- source/board_state.gd
+++ source/board_state.gd
@@ -1,31 +1,31 @@
 extends Node
 
-enum BuildingType {NONE, VILLAGE, CITY}
+enum BuildingType { NONE, VILLAGE, CITY }
 
 var vertices: Dictionary = {}
 var edges: Dictionary = {}
 var robber_hex_pos: Vector2
 
+
 func set_initial_robber_pos(pos: Vector2):
 	robber_hex_pos = Vector2(round(pos.x), round(pos.y))
-	
+
+
 func update_robber_position(pos: Vector2):
 	robber_hex_pos = Vector2(round(pos.x), round(pos.y))
 	print("Estado do Tabuleiro: Ladrão agora está em ", robber_hex_pos)
 
+
 func register_vertices(pos: Vector2):
 	var key = Vector2(round(pos.x), round(pos.y))
 	if not vertices.has(key):
-		vertices[key] = {
-			"owner": null,
-			"type": BuildingType.NONE,
-			"links": []
-		}
+		vertices[key] = {"owner": null, "type": BuildingType.NONE, "links": []}
+
 
 func register_edges(a: Vector2, b: Vector2):
 	var center = (a + b) / 2.0
 	var key = Vector2(round(center.x), round(center.y))
-	
+
 	if not edges.has(key):
 		edges[key] = {
 			"owner": null,
--- source/robber.gd
+++ source/robber.gd
@@ -1,10 +1,14 @@
 extends Sprite2D
 
+
 func moving_to(new_pos: Vector2, immediate: bool = false):
 	if immediate:
 		global_position = new_pos
 	else:
 		var tween = create_tween()
-		tween.tween_property(self, "global_position", new_pos, 0.5)\
-			.set_trans(Tween.TRANS_SINE)\
-			.set_ease(Tween.EASE_OUT)
+		(
+			tween
+			. tween_property(self, "global_position", new_pos, 0.5)
+			. set_trans(Tween.TRANS_SINE)
+			. set_ease(Tween.EASE_OUT)
+		)
