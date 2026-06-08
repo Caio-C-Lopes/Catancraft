bfd5d4df25b3fc6ec4adff5d9c7a3c8f17bddb20
lexguimaraes <alexfsg@id.uff.br>
2026-06-07 23:54:40 -0300

    test(bot): add unit tests for BotController

---
+++ tests/test_bot_controller.gd
@@ -0,0 +1,38 @@
+extends GutTest
+
+var BotController = preload("res://source/bot_controller.gd")
+
+func test_bot_controller_setup():
+	var controller = BotController.new()
+	var mock_gm = Node.new()
+	controller.setup(mock_gm)
+	
+	assert_eq(controller.gm, mock_gm, "BotController should have mock_gm as the gm reference")
+	
+	controller.free()
+	mock_gm.free()
+
+class MockPlayer:
+	var resources = {}
+
+func test_choose_resource_returns_rarest():
+	var controller = BotController.new()
+	var mock_gm = Node.new()
+	
+	var player = MockPlayer.new()
+	player.resources = {
+		"ore": 2,
+		"wheat": 3,
+		"sheep": 1,
+		"wood": 0, 
+		"brick": 5
+	}
+	
+	mock_gm.set("players", [null, player])
+	controller.setup(mock_gm)
+	
+	var rarest = controller.choose_resource(1)
+	assert_eq(rarest, "wood", "should choose wood as monopoly target")
+	
+	controller.free()
+	mock_gm.free()
